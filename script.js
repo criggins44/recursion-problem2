503e39d42d706bebdeb40a0b64e53839bd55a7cc
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-07-01 10:49:41 -0400

    reviewed the sample solution to try and understand the logic

--- script.js
+++ script.js
@@ -3,4 +3,16 @@
 /*
 Write a JavaScript function to compute the sum of an array of integers
 */
+const practiceArray = [1,2,3];
 
+function sumArray(arr) {
+
+  if (arr.length === 1) {
+    return arr[0];
+  }
+  else {
+    return arr.pop() + sumArray(arr);
+  }
+};
+
+console.log(sumArray(practiceArray));
